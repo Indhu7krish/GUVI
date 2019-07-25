a12c5dc43fd1237144f748ebd648c1ce9e8e947f
Indhu7krish <45996086+Indhu7krish@users.noreply.github.com>
2019-07-25 02:12:07 -0700

    Create evenprint.py

---
+++ evenprint.py
@@ -0,0 +1,12 @@
+m,n=list(map(str,input().split()))
+s=0
+l=[]
+if(n.isdigit() and m.isdigit()):
+    n=int(n)
+    m=int(m)
+    for i in range(m+1,n):
+        if(i%2==0):
+            l.append(i)
+    print(*l)
+else:
+    print("Invalid Input")
